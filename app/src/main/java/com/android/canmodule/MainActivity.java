9168967a908b59016d86dbe345e463bb326eedcf
ccm <ccm3447512070@163.com>
2023-11-23 15:00:41 +0800

    上传最新so库，解除MainActivity中的调用

--- app/src/main/java/com/android/canmodule/MainActivity.java
+++ app/src/main/java/com/android/canmodule/MainActivity.java
@@ -15,39 +15,32 @@ import com.android.canlibrary.utils.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Can1DataBean can1DataBean;
-    private Can2DataBean can2DataBean;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        CanInterface canInterface = new CanInterface();
-//        canInterface.openCan1Interface(Constant.DEVICES_CAN1);
-//        canInterface.openCan2Interface(Constant.DEVICES_CAN2);
-//
-//        can1DataBean = new Can1DataBean();
-//        can2DataBean = new Can2DataBean();
-//
-//        Can1ChangeObserver can1ChangeObserver = new Can1ChangeObserver();
-//        can1ChangeObserver.setCanChangeListener(new Can1ChangeObserver.CanChangeListener() {
-//            @Override
-//            public void onCan1Changed(Can1DataBean can1DataBean) {
-//                Logger.d("can1DataBean: "+can1DataBean.getSpeedOfVehicle());
-//
-//            }
-//        });
-//
-//
-//        Can2ChangeObserver can2ChangeObserver = new Can2ChangeObserver();
-//        can2ChangeObserver.setCanChangeListener(new Can2ChangeObserver.CanChangeListener() {
-//            @Override
-//            public void onCan2Changed(Can2DataBean can2DataBean) {
-//
-//                Logger.d("can2DataBean: "+can2DataBean.getTurnLeftLightSignal());
-//            }
-//        });
+        CanInterface canInterface = new CanInterface();
+        canInterface.openCan1Interface(Constant.DEVICES_CAN1);
+        canInterface.openCan2Interface(Constant.DEVICES_CAN2);
+
+        Can1ChangeObserver can1ChangeObserver = new Can1ChangeObserver();
+        can1ChangeObserver.setCanChangeListener(new Can1ChangeObserver.CanChangeListener() {
+            @Override
+            public void onCan1Changed(Can1DataBean can1DataBean) {
+                Logger.d("can1DataBean: "+can1DataBean.getSpeedOfVehicle());
+
+            }
+        });
+
+        Can2ChangeObserver can2ChangeObserver = new Can2ChangeObserver();
+        can2ChangeObserver.setCanChangeListener(new Can2ChangeObserver.CanChangeListener() {
+            @Override
+            public void onCan2Changed(Can2DataBean can2DataBean) {
+
+                Logger.d("can2DataBean: "+can2DataBean.getTurnLeftLightSignal());
+            }
+        });
 
     }
 }
